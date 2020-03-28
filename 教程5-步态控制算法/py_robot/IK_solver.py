ef1b0490c52dcbbf8a9ea1282fa2a4fb62d9c025
Boyang Xing <golaced@163.com>
2020-03-28 12:35:39 +0800

    更新第5章

---
+++ 教程5-步态控制算法/py_robot/IK_solver.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 18:20:45 2020
+
+@author: linux-asd
+"""
+import numpy
+
+def checkdomain(D):
+    if D > 1 or D < -1:
+        print("____OUT OF DOMAIN____")
+        if D > 1: 
+            D = 0.99
+            return D
+        elif D < -1:
+            D = -0.99
+            return D
+    else:
+        return D
+
+def solve_R(coord):#右腿的逆解
+    x = coord[0,1]  #it changes the body_frame to the leg_frame
+    y = -coord[0,2] #(which has different axis)
+    z = -coord[0,0]
+    
+    """LEG DIMENTIONS"""#腿的餐宿
+    coxa = 0.036#跨关节  侧向延长
+    femur = 0.11#大腿
+    tibia = 0.11#小腿
+    D = (x**2+y**2-coxa**2+z**2-femur**2-tibia**2)/(2*tibia*femur)  #siempre <1
+    D = checkdomain(D)
+    gamma = numpy.arctan2(-numpy.sqrt(1-D**2),D)
+    tetta = -numpy.arctan2(-y,x)-numpy.arctan2(numpy.sqrt(x**2+y**2-coxa**2),-coxa)
+    alpha = numpy.arctan2(z,numpy.sqrt(x**2+y**2-coxa**2))-numpy.arctan2(tibia*numpy.sin(gamma),femur+tibia*numpy.cos(gamma))
+    angles = numpy.matrix([-tetta, alpha, gamma])
+    return angles
+
+def solve_L(coord):#左腿逆解
+    x = coord[0,1]  #it changes the body_frame to the leg_frame
+    y = -coord[0,2] #(which has different axis)
+    z = -coord[0,0]
+    
+    """LEG DIMENTIONS"""
+    coxa = -0.036
+    femur = 0.11
+    tibia = 0.11
+    D = (x**2+y**2-coxa**2+z**2-femur**2-tibia**2)/(2*tibia*femur)  #siempre <1
+    D = checkdomain(D)
+    gamma = numpy.arctan2(-numpy.sqrt(1-D**2),D)
+    tetta = -numpy.arctan2(-y,x)-numpy.arctan2(numpy.sqrt(x**2+y**2-coxa**2),-coxa)
+    alpha = numpy.arctan2(z,numpy.sqrt(x**2+y**2-coxa**2))-numpy.arctan2(tibia*numpy.sin(gamma),femur+tibia*numpy.cos(gamma))
+    angles = numpy.matrix([-tetta, alpha, gamma])
+    return angles
